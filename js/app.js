562a6ad80d10ab0ea6a7a4c2ee432630a02cece0
KurKuza <47026225+KurKuza@users.noreply.github.com>
2021-07-18 16:23:22 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -801,7 +801,20 @@ window.onload = function () {
          headerElement.classList.add('_scroll');
       }
    };
+   // Actions (делегирование события click)
+   function documentActions(e) {
+      const targetElement = e.target;
+      if (window.innerWidth > 768 && isMobile.any()) {
+         if (targetElement.classList.contains('menu__arrow')) {
+            targetElement.closest('.menu__item').classList.toggle('_hover');
+         }
+         if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+            _removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover");
+         }
+      }
+   }
 }
+
 
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
